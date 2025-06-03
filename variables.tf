17ac9bb856dd3beef37e3d3af1eccead6fb01c06
comby quentin <quentinc33@yahoo.com>
2025-06-03 16:15:49 +0200

    push

--- variables.tf
+++ variables.tf
@@ -1,10 +1,19 @@
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  default = "rg-terraform-demo"
+}
+
 variable "location" {
   default = "eastus"
 }
-variable "vnet_name" {}
-variable "subnet_name" {}
-variable "admin_username" {}
+variable "vnet_name" {
+    default = "vnet-demo"
+}
+variable "subnet_name" {
+    default = "subnet-demo"
+}
+variable "admin_username" {
+    default = "azureuser"
+}
 variable "admin_password" {
   sensitive = true
 }
